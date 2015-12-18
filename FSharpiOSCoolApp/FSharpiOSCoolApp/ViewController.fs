c18cd193ebef3effd2828b0472070f23cc749954
olegoid <olegoid91@gmail.com>
2015-12-18 01:57:20 -0300

    [FSharpiOSCoolApp] Add F# iOS app to submission samples

---
+++ FSharpiOSCoolApp/FSharpiOSCoolApp/ViewController.fs
@@ -0,0 +1,28 @@
+﻿namespace FSharpiOSCoolApp
+
+open System
+open System.Drawing
+
+open Foundation
+open UIKit
+
+[<Register ("ViewController")>]
+type ViewController (handle:IntPtr) =
+    inherit UIViewController (handle)
+
+    override x.DidReceiveMemoryWarning () =
+        // Releases the view if it doesn't have a superview.
+        base.DidReceiveMemoryWarning ()
+        // Release any cached data, images, etc that aren't in use.
+
+    override x.ViewDidLoad () =
+        base.ViewDidLoad ()
+        // Perform any additional setup after loading the view, typically from a nib.
+
+    override x.ShouldAutorotateToInterfaceOrientation (toInterfaceOrientation) =
+        // Return true for supported orientations
+        if UIDevice.CurrentDevice.UserInterfaceIdiom = UIUserInterfaceIdiom.Phone then
+           toInterfaceOrientation <> UIInterfaceOrientation.PortraitUpsideDown
+        else
+           true
+
